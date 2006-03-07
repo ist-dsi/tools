36b97ffa4278906c00c0b069c22c72bf9bcb134a
Luis Cruz <luis.cruz@ist.utl.pt>
2006-03-07 23:41:21 +0000

    Obtain properties from property file.

---
+++ fenix-tools/src/pt/utl/ist/fenix/tools/util/PropertiesManager.java
@@ -0,0 +1,24 @@
+package pt.utl.ist.fenix.tools.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertiesManager {
+
+	private static final PropertiesManager instance = new PropertiesManager();
+
+    public static Properties loadProperties(final String fileName) throws IOException {
+    	final Properties properties = new Properties();
+    	loadProperties(properties, fileName);
+    	return properties;
+   }
+
+    public static void loadProperties(final Properties properties, final String fileName) throws IOException {
+        final InputStream inputStream = instance.getClass().getResourceAsStream(fileName);
+        if (inputStream != null) {
+            properties.load(inputStream);
+        }
+    }
+
+}
