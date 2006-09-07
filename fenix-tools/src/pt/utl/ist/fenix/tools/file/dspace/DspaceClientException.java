8f94a9a29706359e0e22f7709dbb17300aaf560d
Luis Cruz <luis.cruz@ist.utl.pt>
2006-09-07 14:09:04 +0000

    Revert to previous version.

---
+++ fenix-tools/src/pt/utl/ist/fenix/tools/file/dspace/DspaceClientException.java
@@ -0,0 +1,26 @@
+package pt.utl.ist.fenix.tools.file.dspace;
+
+public class DspaceClientException extends Exception {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    public DspaceClientException() {
+        super();
+    }
+
+    public DspaceClientException(String message) {
+        super(message);
+    }
+
+    public DspaceClientException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DspaceClientException(Throwable cause) {
+        super(cause);
+    }
+
+}
