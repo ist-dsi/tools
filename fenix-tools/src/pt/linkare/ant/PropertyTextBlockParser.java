6ab8dca54712668d2046539e86e5f7e8d1fbda21
José Pereira <jpereira@linkare.com>
2008-05-06 01:31:45 +0000

    continuation line support in properties files

--- fenix-tools/src/pt/linkare/ant/PropertyTextBlockParser.java
+++ fenix-tools/src/pt/linkare/ant/PropertyTextBlockParser.java
@@ -8,64 +8,67 @@ import java.util.List;
 
 public class PropertyTextBlockParser implements TextBlockParser {
 
-    public List<TextBlock> readBlocks(Reader in) throws IOException {
-	List<TextBlock> outList = new ArrayList<TextBlock>();
+	public List<TextBlock> readBlocks(Reader in) throws IOException {
+		List<TextBlock> outList = new ArrayList<TextBlock>();
 
-	BufferedReader br = (in instanceof BufferedReader) ? (BufferedReader) in
-		: new BufferedReader(in);
+		BufferedReader br = (in instanceof BufferedReader) ? (BufferedReader) in
+				: new BufferedReader(in);
 
-	String line = null;
-	String previousContentFinish = null;
-	String startNextContent = null;
+		String line = null;
+		String previousContentFinish = null;
+		String startNextContent = null;
 
-	StringBuffer content = new StringBuffer();
-	while ((line = br.readLine()) != null) {
+		boolean continuation=false;
+		StringBuffer content = new StringBuffer();
+		while ((line = br.readLine()) != null) {
 
-	    if (line.trim().length() == 0)
-		continue;
+			if (line.trim().length() == 0)
+				continue;
 
-	    line=line+CRLF;
-	    if (line.trim().startsWith("#")) {
-		previousContentFinish = "";
-		startNextContent = line;
-	    } else if (line.contains("=")) {
-		previousContentFinish = "";
-		startNextContent = line;
-	    } else {
-		previousContentFinish = line;
-		startNextContent = null;
-	    }
+			line = line + CRLF;
+			if (line.trim().startsWith("#")) {
+				previousContentFinish = "";
+				startNextContent = line;
+			} else if (line.contains("=") && !continuation) {
+				previousContentFinish = "";
+				startNextContent = line;
+			} else {
+				previousContentFinish = line;
+				startNextContent = null;
+			}
+			//while we have a continuation line just keep ignoring a new = sign
+			continuation=line.endsWith("\\");
+			
 
-	    content.append(previousContentFinish);
-	    if (startNextContent != null) {
+			content.append(previousContentFinish);
+			if (startNextContent != null) {
+				if (content.toString().trim().length() != 0) {
+					TextBlock block = null;
+					if (content.toString().trim().startsWith("#"))
+						block = new UnknownTextBlock();
+					else
+						block = new PropertyTextBlock();
 
+					block.setContent(content.toString());
+					outList.add(block);
+				}
+
+				content = new StringBuffer(startNextContent);
+			}
+
+		}
 		if (content.toString().trim().length() != 0) {
-		    TextBlock block = null;
-		    if (content.toString().trim().startsWith("#"))
-			block = new UnknownTextBlock();
-		    else
-			block = new PropertyTextBlock();
+			TextBlock block = null;
+			if (content.toString().trim().startsWith("#"))
+				block = new UnknownTextBlock();
+			else
+				block = new PropertyTextBlock();
 
-		    block.setContent(content.toString());
-		    outList.add(block);
+			block.setContent(content.toString());
+			outList.add(block);
 		}
 
-		content = new StringBuffer(startNextContent);
-	    }
-	   
+		return outList;
 	}
-	 if (content.toString().trim().length() != 0) {
-		TextBlock block = null;
-		if (content.toString().trim().startsWith("#"))
-		    block = new UnknownTextBlock();
-		else
-		    block = new PropertyTextBlock();
-
-		block.setContent(content.toString());
-		outList.add(block);
-	    }
-
-	return outList;
-    }
 
 }
