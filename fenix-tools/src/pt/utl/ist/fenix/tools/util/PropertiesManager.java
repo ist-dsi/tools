7078f7b9f10ee9fcf95f791d5992f6d51afccf7c
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2009-02-06 17:29:10 +0000

    Support to load properties giving a absolute file path.

--- fenix-tools/src/pt/utl/ist/fenix/tools/util/PropertiesManager.java
+++ fenix-tools/src/pt/utl/ist/fenix/tools/util/PropertiesManager.java
@@ -1,5 +1,7 @@
 package pt.utl.ist.fenix.tools.util;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -14,6 +16,12 @@ public class PropertiesManager {
 	return properties;
     }
 
+    public static Properties loadPropertiesFromFile(final String filePath) throws IOException {
+	final Properties properties = new Properties();
+	loadPropertiesFromFile(properties, filePath);
+	return properties;
+    }
+
     public static void loadProperties(final Properties properties, final String fileName) throws IOException {
 	final InputStream inputStream = instance.getClass().getResourceAsStream(fileName);
 	if (inputStream != null) {
@@ -21,4 +29,11 @@ public class PropertiesManager {
 	}
     }
 
+    public static void loadPropertiesFromFile(final Properties properties, final String filePath) throws IOException {
+	final File file = new File(filePath);
+	final InputStream inputStream = new FileInputStream(file);
+	if (inputStream != null) {
+	    properties.load(inputStream);
+	}
+    }
 }
