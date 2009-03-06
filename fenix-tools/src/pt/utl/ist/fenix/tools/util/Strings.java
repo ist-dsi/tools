cb4fd8bfd43820cc2ae952063167325760807dc3
Luis Cruz <luis.cruz@ist.utl.pt>
2009-03-06 18:46:57 +0000

    Methods for internalizing and externalizing value type.

--- fenix-tools/src/pt/utl/ist/fenix/tools/util/Strings.java
+++ fenix-tools/src/pt/utl/ist/fenix/tools/util/Strings.java
@@ -1,9 +1,11 @@
 package pt.utl.ist.fenix.tools.util;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 public class Strings extends ObjectList<String> implements Serializable {
 
@@ -31,5 +33,37 @@ public class Strings extends ObjectList<String> implements Serializable {
 	}
 	return false;
     }
-    
+ 
+    public String exportAsString() {
+	StringBuilder buffer = new StringBuilder("");
+	for (String string : this) {
+	    buffer.append(string.length());
+	    buffer.append(":");
+	    buffer.append(string);
+	}
+	return buffer.toString();
+    }
+
+    public static Strings importFromString(String string) {
+	if (string == null) {
+	    return null;
+	}
+	List<String> strings = new ArrayList<String>();
+
+	int beginIndex = 0;
+	int endIndex = find(beginIndex, ':', string);
+
+	while (beginIndex >= 0 && endIndex > beginIndex) {
+	    int size = Integer.valueOf(string.substring(beginIndex, endIndex++));
+	    strings.add(string.substring(endIndex, endIndex + size));
+	    beginIndex = endIndex + size;
+	    endIndex = beginIndex + find(beginIndex, ':', string);
+	}
+	return new Strings(strings);
+    }
+
+    private static int find(int index, char c, String string) {
+	return string.substring(index).indexOf(c);
+    }
+
 }
