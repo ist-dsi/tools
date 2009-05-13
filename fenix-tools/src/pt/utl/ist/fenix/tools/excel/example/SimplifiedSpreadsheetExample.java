b2203ab6df4c55be0debca36d58405a4366a6b2c
João Figueiredo <joao.figueiredo@ist.utl.pt>
2009-05-13 16:15:09 +0000

    corrected example

--- fenix-tools/src/pt/utl/ist/fenix/tools/excel/example/SimplifiedSpreadsheetExample.java
+++ fenix-tools/src/pt/utl/ist/fenix/tools/excel/example/SimplifiedSpreadsheetExample.java
@@ -2,13 +2,13 @@ package pt.utl.ist.fenix.tools.excel.example;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
 
 import org.joda.time.LocalDate;
 
 import pt.utl.ist.fenix.tools.excel.CellConverter;
 import pt.utl.ist.fenix.tools.excel.SimplifiedSpreadsheetBuilder;
 import pt.utl.ist.fenix.tools.excel.WorkbookExportFormat;
+import pt.utl.ist.fenix.tools.excel.styles.CellDataFormat;
 
 public class SimplifiedSpreadsheetExample {
     public static class SomeBean {
@@ -32,14 +32,17 @@ public class SimplifiedSpreadsheetExample {
 	elements.add(new SomeBean());
 	elements.add(new SomeBean());
 	elements.add(new SomeBean());
+
 	SimplifiedSpreadsheetBuilder<SomeBean> builder = new SimplifiedSpreadsheetBuilder<SomeBean>(elements) {
 	    {
 		addConverter(LocalDate.class, new CellConverter() {
 		    @Override
 		    public Object convert(Object source) {
-			return new Date(((LocalDate) source).toDateTimeAtCurrentTime().getMillis());
+			return ((LocalDate) source).toDateTimeAtCurrentTime().toDate();
 		    }
 		});
+		
+		addTypeStyle(LocalDate.class, new CellDataFormat("dd/MM/yyyy"));
 	    }
 
 	    @Override
