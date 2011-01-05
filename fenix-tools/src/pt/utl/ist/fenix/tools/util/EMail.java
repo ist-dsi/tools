d0cc200e6d687d567e7254d196c53af3073bcaca
Luis Cruz <luis.cruz@ist.utl.pt>
2011-01-05 09:52:14 +0000

    Bug Fix: Comply to RFC specification for validating email addresses.

--- fenix-tools/src/pt/utl/ist/fenix/tools/util/EMail.java
+++ fenix-tools/src/pt/utl/ist/fenix/tools/util/EMail.java
@@ -27,7 +27,7 @@ public class EMail implements Serializable {
 
     private String Origem;
 
-    public static String W3C_EMAIL_SINTAX_VALIDATOR = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*$";
+    public static String W3C_EMAIL_SINTAX_VALIDATOR = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-!#$%&'*+/=?^_`{|}~]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*$";
 
     /**
      * Metodo Construtor, recebe um Servidor e uma Origem por omissao
