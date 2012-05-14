acf6a45ea9cf2f0537df1501449ed2ac5f2c9758
David Martinho <davidmartinho@ist.utl.pt>
2012-05-14 15:44:38 +0000

    New Feature: Migrated the ByteArray value type to fenix tools from Bennu and FilePlugin

---
+++ fenix-tools/src/pt/utl/ist/fenix/tools/util/ByteArray.java
@@ -0,0 +1,28 @@
+package pt.utl.ist.fenix.tools.util;
+
+import java.io.Serializable;
+
+/**
+ * 
+ * @author  Nuno Diegues
+ * @author  Anil Kassamali
+ * @author  Luis Cruz
+ * 
+*/
+public class ByteArray implements Serializable {
+
+    private byte[] bytes;
+
+    public ByteArray(byte[] value) {        
+        this.bytes = value;
+    }
+
+    public byte[] getBytes() {
+        return bytes;
+    }
+
+    public void setBytes(byte[] bytes) {
+        this.bytes = bytes;
+    }
+
+}
